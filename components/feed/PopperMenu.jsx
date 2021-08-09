a5ddbabe98ec68276e5473eca79f1f822cda0454
huiji0315 <huiji0315@gmail.com>
2021-08-10 02:11:14 +0900

    수업 4차시 미션 초본 답안

--- components/feed/PopperMenu.jsx
+++ components/feed/PopperMenu.jsx
@@ -20,9 +20,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const PopperMenu = (
-  {
-    /* 알맞은 props를 전달해주세요. */
-  }
+  { deleteHandler, feedUid }
 ) => {
   const classes = useStyles();
   const router = useRouter();
--- pages/feed/[feedUid].js
+++ pages/feed/[feedUid].js
@@ -16,35 +16,37 @@ const useStyles = makeStyles((theme) => ({
 export const getServerSideProps = async (context) => {
   const { feedUid } = context.query;
   /* 1.유저의 정보를 가져오기 위해 알맞은 api 요청을 보내주세요. */
-  const fetchUserInfo = await fetch(/* 채워주세요. */);
+  const fetchUserInfo = await fetch("http://localhost:3000/api/user");
   const user = await fetchUserInfo.json();
 
   /* 2.한 피드의 디테일 정보를 가져오기 위해 알맞은 api 요청을 보내주세요. */
-  const fetchFeedDetail = await fetch(/* 채워주세요. */);
+  const fetchFeedDetail = await fetch(`http://localhost:3000/api/feed/${feedUid}`);
   const feedDetail = await fetchFeedDetail.json();
 
   /* 3.전달해야 할 props를 return 해주세요. (3가지) */
   return {
     props: {
-      /* 채워주세요. */
+      feedUid,
+      user,
+      feedDetail,
     },
   };
 };
 
-const detail = ({/* 알맞은 props를 전달해주세요. */}) => {
+const detail = ({ feedUid, user, feedDetail }) => {
   const classes = useStyles();
   const router = useRouter();
 
   async function deleteFeed() {
     try {
       /* 1.유저의 피드를 삭제하기 위해 알맞은 api 요청을 보내주세요. (옵션 필요) */
-      const deleteResult = await fetch( /* 채워주세요. */, { method:  /* 채워주세요. */ });
+      const deleteResult = await fetch(`/api/feed/${feedUid}?userId=${feedDetail.author.uid}`, { method: "DELETE" });
       const { message } = await deleteResult.json();
 
       /* 2.정상적으로 삭제 후, 메인 피드 화면으로 이동하게 해주세요. query 값으로 message를 보내주세요. */
       router.push({
-        pathname:/* 채워주세요. */,
-        query: /* 채워주세요. */,
+        pathname: "/feed",
+        query: { message },
       });
     } catch (e) {
       console.error(e);
@@ -55,7 +57,7 @@ const detail = ({/* 알맞은 props를 전달해주세요. */}) => {
     <Grid container className={classes.container}>
       <Grid item xs={8} className={classes.feed}>
         {/* 알맞은 props를 전달해주세요. */} 
-        <DetailFeed feed={/* 채워주세요. */} deleteHandler={/* 채워주세요. */} user={/* 채워주세요. */} />
+        <DetailFeed feed={feedDetail} deleteHandler={deleteFeed} user={user} />
       </Grid>
       <Grid item xs={4}>
         <Comment user={user} feedType={"detail"} />
